caa5687403a050b69e40e96c154dd7b1ac3f7e3b
Fad <fadhilerdyaqasmal@gmail.com>
2024-06-17 22:16:04 +0700

    fix navbar

--- app/src/main/java/com/example/calmify/views/MainActivity.java
+++ app/src/main/java/com/example/calmify/views/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.calmify.views;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import com.example.calmify.R;
+import com.example.calmify.views.fragments.HomeFragment;
+import com.example.calmify.views.fragments.InProgressFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +30,34 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navigation_home) {
+                    selectedFragment = new HomeFragment();
+                } else if (itemId == R.id.navigation_meditate) {
+                    selectedFragment = new InProgressFragment();
+                } else if (itemId == R.id.navigation_activity) {
+                    selectedFragment = new InProgressFragment();
+                } else if (itemId == R.id.navigation_community) {
+                    selectedFragment = new InProgressFragment();
+                }else{
+                    Log.d(TAG, "onNavigationItemSelected: "+ itemId);
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, selectedFragment).commit();
+                }
+                return true;
+            }
         });
     }
 }

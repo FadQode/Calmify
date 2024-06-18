7baae48505b7dac153a342e4a4d48642c4b6e330
Fad <fadhilerdyaqasmal@gmail.com>
2024-06-18 12:50:12 +0700

    add a stopwatch

--- app/src/main/java/com/example/calmify/views/SleepActivity.java
+++ app/src/main/java/com/example/calmify/views/SleepActivity.java
@@ -1,29 +1,98 @@
 package com.example.calmify.views;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.example.calmify.R;
+import com.google.android.material.button.MaterialButton;
+import java.text.MessageFormat;
+import java.util.Locale;
 
 public class SleepActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button reset, start, stop;
+    int hours, seconds, minutes, milliSeconds;
+    long millisecondTime, startTime, timeBuff, updateTime = 0L;
+    Handler handler;
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            millisecondTime = SystemClock.uptimeMillis() - startTime;
+            updateTime = timeBuff + millisecondTime;
+
+            seconds = (int) (updateTime / 1000);
+            minutes = seconds / 60;
+            hours = minutes / 60;
+
+            minutes = minutes % 60;
+            seconds = seconds % 60;
+            milliSeconds = (int) (updateTime % 1000);
+
+            textView.setText(MessageFormat.format("{0}:{1}:{2}:{3}",
+                    hours,
+                    String.format(Locale.getDefault(), "%01d", minutes),
+                    String.format(Locale.getDefault(), "%02d", seconds),
+                    String.format(Locale.getDefault(), "%03d", milliSeconds)
+            ));
+
+            handler.postDelayed(this, 0);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sleep);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-
-
-
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textView = findViewById(R.id.textView);
+        reset = findViewById(R.id.reset);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+
+        handler = new Handler(Looper.getMainLooper());
+
+        start.setOnClickListener(view -> {
+            startTime = SystemClock.uptimeMillis();
+            handler.postDelayed(runnable, 0);
+            reset.setEnabled(false);
+            stop.setEnabled(true);
+            start.setEnabled(false);
+        });
+
+        stop.setOnClickListener(view -> {
+            timeBuff += millisecondTime;
+            handler.removeCallbacks(runnable);
+            reset.setEnabled(true);
+            stop.setEnabled(false);
+            start.setEnabled(true);
+        });
+
+        reset.setOnClickListener(view -> {
+            millisecondTime = 0L;
+            startTime = 0L;
+            timeBuff = 0L;
+            updateTime = 0L;
+            hours = 0;
+            seconds = 0;
+            minutes = 0;
+            milliSeconds = 0;
+            textView.setText("00:00:000");
+        });
+
+        textView.setText("00:00:000");
     }
 }
--- app/src/main/java/com/example/calmify/views/fragments/HomeFragment.java
+++ app/src/main/java/com/example/calmify/views/fragments/HomeFragment.java
@@ -1,14 +1,26 @@
 package com.example.calmify.views.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.calmify.R;
+import com.example.calmify.views.MainActivity;
+import com.example.calmify.views.SignInActivity;
+import com.example.calmify.views.SignUpActivity;
+import com.example.calmify.views.SleepActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,7 +72,17 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        ImageView sleepLogo;
+
+        sleepLogo = view.findViewById(R.id.sleepLogo);
+
+        sleepLogo.setOnClickListener(v -> {
+
+            Intent intent = new Intent(getActivity(), SleepActivity.class);
+            startActivity(intent);
+        });
+        return view;
     }
 }
